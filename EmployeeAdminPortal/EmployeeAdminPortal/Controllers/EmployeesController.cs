b9c9e7f8f893d985430d72e882e5828b9143a90e
siddhant ashok <siddhant.ashok@gingerwebs.in>
2024-09-18 18:48:21 +0530

    EmployeesController with GetEmployees Endpoint

---
+++ EmployeeAdminPortal/EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeAdminPortal.Data;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    // localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public ApplicationDBContext dbContext { get; }
+
+        public EmployeesController(ApplicationDBContext dbContext)
+        { 
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+            return Ok(allEmployees);
+            //return 
+        }
+    }
+}
